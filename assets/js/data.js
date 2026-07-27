e0521f009dc50544d010c599ebadfa50331a81ac
dhimasaditya366 <75363189+dhimasaditya366@users.noreply.github.com>
2026-07-27 14:21:37 +0700

    Update konten via Admin Dashboard (27/7/2026, 14.21.36)

--- assets/js/data.js
+++ assets/js/data.js
@@ -14,7 +14,7 @@ window.SITE_DEFAULTS = {
         "hours": {
             "weekday": "08.00 - 17.00",
             "saturday": "08.00 - 14.00",
-            "sunday": "Tutup"
+            "sunday": "08.00 - 14.00"
         },
         "taglines": [
             "Kualitas Terjamin",
